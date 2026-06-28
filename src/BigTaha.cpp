5053a932507427f63198f37514d53bb0d14441ba
abolfazl <maleki.direct@gmail.com>
2026-06-28 10:15:19 +0330

    Add Normal Ability 2 BigTaha.cpp

--- src/BigTaha.cpp
+++ src/BigTaha.cpp
@@ -23,3 +23,64 @@ void BigTaha::NormalAbility1(AbilityContext& context)
     cout<<"******** Ragbar execute successfully ********"<<endl;
 
 }
+
+
+
+
+void BigTaha::NormalAbility2(AbilityContext& context)
+{
+    vector<string> Enemy;
+    int counter = 1;
+
+    cout<<"Live enemy heroes: "<<endl;
+    for(const auto &hero : context.LivingEnemyHeroes)
+    {
+        if(hero->IsHiddenFunc())
+            continue;
+
+        cout<<counter<<") "<<hero->Name<<"\t";
+        Enemy.push_back(hero->Name);
+        counter++;
+    }
+
+    int target;
+    while(true)
+    {
+        cout<<"\nPlease enter your choice: ";
+        cin>>target;
+
+        if(target < 1 || target > context.LivingEnemyHeroes.size())
+        {
+            cout<<"Invalid target. enter again!";
+            continue;
+        }
+        else
+            break;
+    }
+
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for(const auto &hero : context.LivingEnemyHeroes)
+    {
+        if(hero->Name == Enemy[target - 1])
+        {
+            hero->TakeDamage(90, IsDamageMultiplierEnabled, DamageType::Single, *context.GameObj);
+            if (hero->GetCurrentHP() <= 0)
+            {
+                hero->SetDead();
+            }
+
+            SetHidden();
+            EndHiddenRound = context.RoundCount + 1;
+
+            break;
+        }
+    }
+
+    context.MyTeam->Energy -= 4;
+
+    cout<<"******** Xray execute successfully ********"<<endl;
+
+}
+
