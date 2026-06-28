3234d277416b24bca25f501bdf2d50d1a6078abd
abolfazl <maleki.direct@gmail.com>
2026-06-28 10:22:10 +0330

    Add Normal Ability 2 to AghaShahriar.cpp

--- src/AghaShahriar.cpp
+++ src/AghaShahriar.cpp
@@ -73,3 +73,73 @@ void AghaShahriar::NormalAbility1(AbilityContext& context)
     cout<<"******** Maskhare execute successfully ********"<<endl;
 }
 
+
+void AghaShahriar::NormalAbility2(AbilityContext& context)
+{
+    random_device rd;
+    mt19937 generator(rd());
+
+    vector<string> Enemy;
+    int counter = 1;
+
+    cout<<"Live enemy heroes: "<<endl;
+    for(const auto &hero : context.LivingEnemyHeroes)
+    {
+        if(hero->IsHiddenFunc())
+            continue;
+
+        cout<<counter<<") "<<hero->Name<<"\t";
+        Enemy.push_back(hero->Name);
+        counter++;
+    }
+
+    int target;
+    while(true)
+    {
+        cout<<"\nPlease enter your choice: ";
+        cin>>target;
+
+        if(target < 1 || target > context.LivingEnemyHeroes.size())
+        {
+            cout<<"Invalid target. enter again!";
+            continue;
+        }
+        else
+            break;
+    }
+
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<Hero*> ChooseEnemies;
+
+    for(const auto &hero : context.LivingEnemyHeroes)
+    {
+        if(hero->Name != Enemy[target - 1])
+        {
+            ChooseEnemies.push_back(hero);
+        }
+    }
+
+    if(ChooseEnemies.empty())
+    {
+        cout<<"There is no enemy.";
+        return;
+    }
+
+    uniform_int_distribution<int> RandIndex(0, ChooseEnemies.size() - 1);
+    int randomIndex = RandIndex(generator);
+
+    ChooseEnemies[randomIndex]->TakeDamage(100, IsDamageMultiplierEnabled, DamageType::Single, *context.GameObj);
+    if (ChooseEnemies[randomIndex]->GetCurrentHP() <= 0)
+    {
+        ChooseEnemies[randomIndex]->SetDead();
+    }
+
+
+    context.MyTeam->Energy -= 4;
+
+    cout<<"******** Lajbaz execute successfully ********"<<endl;
+}
+
+
