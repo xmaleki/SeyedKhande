fe4ed68854e5f2f25667e0943942d2def873d08b
abolfazl <maleki.direct@gmail.com>
2026-06-28 10:29:03 +0330

    Add GetNameOfAbility() function to Hero.cpp soruce file.

--- src/Hero.cpp
+++ src/Hero.cpp
@@ -11,3 +11,10 @@ int Hero::GetHP() const
     return CurrentHP;
 }
 
+
+void Hero::GetNameOfAbility() const
+{
+    cout<<"Normal Ability: "<<NameOfNormalAbility1<<"\t"<<NameOfNormalAbility2<<endl;
+    cout<<"Special Ability: "<<NameOfSpecialAbility<<endl;
+}
+
