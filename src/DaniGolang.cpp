98e256fbdeb5d296bbf2a7db3444163a394bc39f
abolfazl <maleki.direct@gmail.com>
2026-06-28 10:08:00 +0330

    Add Normal ability 2 to DaniGolang.cpp soruce file

--- src/DaniGolang.cpp
+++ src/DaniGolang.cpp
@@ -82,3 +82,80 @@ void DaniGolang::NormalAbility1(AbilityContext& context)
 
 }
 
+
+void DaniGolang::NormalAbility2(AbilityContext& context)
+{
+    Hero* MaxHpHero = context.LivingEnemyHeroes[0];
+    
+    int MaximumHp = context.LivingEnemyHeroes[0]->GetHP();
+
+    for(int i = 1; i < context.LivingEnemyHeroes.size() ; i++)
+    {
+        if(context.LivingEnemyHeroes[i]->GetHP() > MaximumHp)
+        {
+            MaximumHp = context.LivingEnemyHeroes[i]->GetHP();
+            MaxHpHero = context.LivingEnemyHeroes[i];
+        }
+    }
+
+    MaxHpHero->TakeDamage(50, IsDamageMultiplierEnabled,DamageType::Single, *context.GameObj);
+    if (MaxHpHero->GetCurrentHP() <= 0)
+    {
+        MaxHpHero->SetDead();
+    }
+
+
+    vector<string> Enemy;
+    int counter = 1;
+
+    cout<<"Live enemy heroes: "<<endl;
+    for(const auto &hero : context.LivingEnemyHeroes)
+    {
+        if(hero->IsHiddenFunc())
+            continue;
+
+        if(hero != nullptr)
+        {
+            cout<<counter<<") "<<hero->Name<<"\t";
+            Enemy.push_back(hero->Name);
+            counter++;
+        }
+    }
+
+    int target;
+    while(true)
+    {
+        cout<<endl<<"Please enter your target: ";
+        cin>>target;
+
+        if(target < 1 || target > context.LivingEnemyHeroes.size())
+        {
+            cout<<"Invalid choice. enter again!";
+            continue;
+        }
+        else
+            break;
+    }
+    
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for(const auto &enemy : context.LivingEnemyHeroes)
+    {
+        if(enemy != nullptr && enemy->Name == Enemy[target - 1])
+        {
+            enemy->TakeDamage(50, IsDamageMultiplierEnabled, DamageType::Single, *context.GameObj);
+            if(enemy->GetCurrentHP() <= 0)
+            {
+                enemy->SetDead();
+            }
+            break;
+        }
+    }
+
+    
+    context.MyTeam->Energy -= 4;
+
+    cout<<"******** Fil Kosh execute successfully ********"<<endl;
+}
+
